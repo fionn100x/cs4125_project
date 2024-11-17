5b3846ca2e662b6e6b6dedf6576cb31cc6cc5876
AmalJohnson44 <johnsonamal65@gmail.com>
2024-11-17 21:41:25 +0000

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,11 +1,15 @@
-# This file contains some variable names you need to use in overall project. 
-#For example, this will contain the name of dataframe columns we will working on each file
+# Config.py
+
 class Config:
     # Input Columns
     TICKET_SUMMARY = 'Ticket Summary'
     INTERACTION_CONTENT = 'Interaction content'
 
-    # Type Columns to test
-    TYPE_COLS = ['y2', 'y3', 'y4']
-    CLASS_COL = 'y2'
-    GROUPED = 'y1'
+    # Type Columns to test (if needed for different label testing)
+    TYPE_COLS = ['Type 1', 'Type 2', 'Type 3', 'Type 4']
+    
+    # Choose the main label column for classification (update this based on your requirement)
+    CLASS_COL = 'Type 1'  # Replace with 'Type 2', 'Type 3', or 'Type 4' if needed
+    
+    # Grouping column if applicable
+    GROUPED = 'Innso TYPOLOGY_TICKET'
