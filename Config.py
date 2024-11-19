5a3fe55e86071b5025f14605967d7c37643467cc
AmalJohnson44 <johnsonamal65@gmail.com>
2024-11-19 19:48:48 +0000

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,9 +3,7 @@ class Config:
     TICKET_SUMMARY = 'Ticket Summary'
     INTERACTION_CONTENT = 'Interaction content'
 
-    # Type Columns to test (update as needed)
-    TYPE_COLS = ['Type 1', 'Type 2', 'Type 3', 'Type 4']  # Ensure consistency based on dataset
-    CLASS_COL = 'Type 1'  # Replace with 'Type 2', 'Type 3', or 'Type 4' as required
-    
-    # Grouping column if applicable
-    GROUPED = 'Innso TYPOLOGY_TICKET'  # Ensure this matches your dataset
+    # Type Columns to test
+    TYPE_COLS = ['y2', 'y3', 'y4']
+    CLASS_COL = 'y2'
+    GROUPED = 'y1'
